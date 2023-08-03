d51214c9e8ea89f048bb95b37ec1377c06a8f4f8
Andriy Panasyk <panasiukmail@gmail.com>
2023-08-03 13:39:42 +0300

    feat: addaptive

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { Row, Col } from "antd"
 import { WhatsAppOutlined } from '@ant-design/icons'
 import './header.scss'
 import Trust from '../../assets/icons/Trust.svg';
@@ -7,63 +6,59 @@ import VectorArrow from '../../assets/icons/Vector9.svg';
 export const Header = () => {
     return (
         <header className="header">
-            <Row justify={"space-evenly"}>
-                <Col span={4 / 5}>
-                    <div className="header-container">
-                        <div className="box">
-                            <div className="box__title">
-                                <span>Take control of your finances with our reliable servicess</span>
-                            </div>
-                            <div className="box__subtitle">
-                                <span>Financial planning, tax consultancy and accounting solutions</span>
-                            </div>
+            <div className="header-container">
+                <div className="box">
+                    <div className="box__title">
+                        <span>Take control of your finances with our reliable servicess</span>
+                    </div>
+                    <div className="box__subtitle">
+                        <span>Financial planning, tax consultancy and accounting solutions</span>
+                    </div>
+                </div>
+                <div className="header-button">
+                    <button className="buttonLarge">
+                        Learn More
+                    </button>
+                </div>
+            </div>
+            <div className="right">
+                <div className="right-box">
+                    <div className="right-container first">
+                        <div className="right-container__image">
+                            <img src={Trust} alt="trust" className="image-wrapp" />
                         </div>
-                        <div className="header-button">
-                            <button className="buttonLarge">
-                                Learn More
-                            </button>
+                        <div className="right-container__text">
+                            Trusted by Companies
+                        </div>
+                        <div className="vector-arrow">
+                            <img src={VectorArrow} alt="vectorArrow" className="image-wrapp" />
                         </div>
                     </div>
-                </Col>
-                <Col span={1 / 5} className="right">
-                    <div className="right-box">
-                        <div className="right-container first">
-                            <div className="right-container__image">
-                                <img src={Trust} alt="trust" className="image-wrapp" />
-                            </div>
-                            <div className="right-container__text">
-                                Trusted by Companies
-                            </div>
-                            <div className="vector-arrow">
-                                <img src={VectorArrow} alt="vectorArrow" className="image-wrapp" />
-                            </div>
+                    <div className="right-container second">
+                        <div className="right-container__number">
+                            100+
                         </div>
-                        <div className="right-container second">
-                            <div className="right-container__number">
-                                100+
-                            </div>
-                            <div className="right-container__text">
-                                Satisfied Clients
-                            </div>
-                            <div className="vector-arrow">
-                                <img src={VectorArrow} alt="vectorArrow" className="image-wrapp" />
-                            </div>
+                        <div className="right-container__text">
+                            Satisfied Clients
+                        </div>
+                        <div className="vector-arrow">
+                            <img src={VectorArrow} alt="vectorArrow" className="image-wrapp" />
                         </div>
                     </div>
-                    <div className="right-container third">
-                        <div className="right-container__text third__text">
-                            Get quick free
-                            сonsultations
-                        </div>
-                        <div className="right-container__button">
-                            <button className="right-header-button">
-                                <WhatsAppOutlined />
-                                Contact Us
-                            </button>
-                        </div>
+                </div>
+                <div className="right-container third">
+                    <div className="right-container__text third__text">
+                        Get quick free
+                        сonsultations
                     </div>
-                </Col>
-            </Row>
+                    <div className="right-container__button">
+                        <button className="right-header-button">
+                            <WhatsAppOutlined />
+                            Contact Us
+                        </button>
+                    </div>
+                </div>
+            </div>
         </header >
     )
 }
